f1df37a10d75cbf090e6dc500deaae4e383d358a
Jacques Oosthuizen <jacques@bluverve.com>
2023-02-08 12:57:44 +0200

    Revert

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', loadChildren: () => import('./app.module').then(m => m.AppModule) },
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
